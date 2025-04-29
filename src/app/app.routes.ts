6faee29fea15739d4fa4995b2bf48fc4cffb0db0
SethuMehansa <sethumisethumi@gmail.com>
2025-04-29 12:45:17 +0530

    remove unwanted component and re-route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,25 +1,10 @@
 import { Routes } from '@angular/router';
-import { UserLoginComponent } from './components/user-login/user-login.component';
-import { UserSignupComponent } from './components/user-signup/user-signup.component';
 import { ManageEmployeeComponent } from './components/manage-employee/manage-employee.component';
 
 export const routes: Routes = [
     {
         path: '',
-        component: UserLoginComponent,
-        pathMatch: 'full'
-      },
-      {
-        path: 'user-signup',
-        component: UserSignupComponent
-      },
-      {
-        path: 'manage-employee',
-        component: ManageEmployeeComponent
-      },
-      {
-        path: '**',
-        redirectTo: '',
+        component: ManageEmployeeComponent,
         pathMatch: 'full'
       }
 ];
