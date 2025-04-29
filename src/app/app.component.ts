2b40ef178662753682565dccc7c05cb3880acca3
SethuMehansa <sethumisethumi@gmail.com>
2025-04-29 11:23:10 +0530

    install tailwind css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { initFlowbite } from 'flowbite';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'EmployeeDirectory-Frontend';
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
